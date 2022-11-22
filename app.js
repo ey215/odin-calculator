7597892b582ff9a2bdef1aface9080f4840284c2
Taylor Mastin <thmastin@gmail.com>
2022-11-21 23:19:44 -0500

    Add functions for basic math operators

---
+++ app.js
@@ -0,0 +1,13 @@
+//add functions for basic math operators
+
+const add = (a, b) => a + b;
+
+function subtract(a, b) {
+  return a - b;
+}
+
+const multiply = (a, b) => a * b;
+
+function divide(a, b) {
+  return a / b;
+}
