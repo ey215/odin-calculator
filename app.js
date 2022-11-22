fc52f7d2808a37df16aff13cea87581788f506c6
Taylor Mastin <thmastin@gmail.com>
2022-11-21 23:21:37 -0500

    Change function to arrow notation

--- app.js
+++ app.js
@@ -1,13 +1,6 @@
 //add functions for basic math operators
 
 const add = (a, b) => a + b;
-
-function subtract(a, b) {
-  return a - b;
-}
-
+const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
-
-function divide(a, b) {
-  return a / b;
-}
+const divide = (a, b) => a / b;
