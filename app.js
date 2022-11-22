aefcbefda033f577e62bac621ba605c40c3691f8
Taylor Mastin <thmastin@gmail.com>
2022-11-21 23:28:24 -0500

    Add function to to get operand and two numbers and do the math

--- app.js
+++ app.js
@@ -4,3 +4,18 @@ const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
 const divide = (a, b) => a / b;
+
+//functin that take operator and two numbers and
+//preforms a arithmetic function on the nubers based on the operator
+
+function operate(operator, num1, num2) {
+  if (operator === "+") {
+    return add(num1, num2);
+  } else if (operator === "-") {
+    return subtract(num1, num2);
+  } else if (operator === "*") {
+    return multiply(num1, num2);
+  } else if (operator === "/") {
+    return divide(num1, num2);
+  }
+}
